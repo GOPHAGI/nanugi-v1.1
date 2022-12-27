4b195f7fb052e74c91ee200a432bf0d62be8a141
khj <kim37171@cuk.edu>
2022-12-27 23:18:26 +0900

    first commit

---
+++ src/test/java/com/gophagi/nanugi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gophagi.nanugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
