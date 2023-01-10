e7052a123a4a4db81825004e0abb8f7d6bb88cc2
khj <kim37171@cuk.edu>
2023-01-10 23:09:45 +0900

    feat: 공동구매 관련 에러코드 추가

--- src/main/java/com/gophagi/nanugi/common/excepion/ErrorCode.java
+++ src/main/java/com/gophagi/nanugi/common/excepion/ErrorCode.java
@@ -7,20 +7,26 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    //Common
-    INVALID_INPUT_VALUE(400, "COMMON-ERR-400", " Invalid Input Value"),
-    HANDLE_ACCESS_DENIED(403, "COMMON-ERR-403", "Access is Denied"),
-    NOT_FOUND(404,"COMMON-ERR-404","PAGE NOT FOUND"),
-    METHOD_NOT_ALLOWED(405, "COMMON-ERR-405", " Invalid Input Value"),
-    INTER_SERVER_ERROR(500,"COMMON-ERR-500","INTER SERVER ERROR"),
+	//Common
+	INVALID_INPUT_VALUE(400, "400 Bad Request", "잘못된 문법으로 인하여 서버가 요청하여 이해할 수 없습니다."),
+	HANDLE_ACCESS_DENIED(403, "403 Forbidden", "해당 컨텐츠에 접근할 권한이 없습니다."),
+	NOT_FOUND(404, "404 Not Found", "PAGE NOT FOUND"),
+	METHOD_NOT_ALLOWED(405, "405 Method Not Allowed", "허용하지 않는 요청 Method 입니다."),
+	INTER_SERVER_ERROR(500, "500 Internal Server Error", "INTER SERVER ERROR"),
 
-    //Member
-    MEMBER_DUPLICATION(400,"MEMBER-ERR-400","MEMBER DUPLICATED");
+	//Member
+	MEMBER_DUPLICATION(500, "500 MEMBER DUPLICATION", "이미 존재하는 회원정보입니다."),
 
+	//Groupbuying
+	CANNOT_DELETE_BOARD(500, "500 CANNOT DELETE BOARD", "공동구매 게시글을 삭제할 수 없습니다."),
+	PARTICIPATION_DUPLICATION(500, "500 PARTICIPATION_DUPLICATION", "중복된 참여 신청입니다."),
+	PERSONNEL_LIMIT_EXCEEDED(500, "500 PERSONNEL_LIMIT_EXCEEDED", "제한 인원 초과하였습니다."),
+	INVALID_PARTICIPANT_INSTANCE(500, "500 INVALID_PARTICIPANT_INSTANCE", "참여자 인스턴스를 생성할 수 없습니다."),
+	INVALID_BOARD_INSTANCE(500, "500 INVALID_BOARD_INSTANCE", "공동구매 보드 인스턴스를 생성할 수 없습니다.")
+	;
 
-    private int status;
-    private String errorCode;
-    private String message;
-
+	private int status;
+	private String errorCode;
+	private String message;
 
 }
