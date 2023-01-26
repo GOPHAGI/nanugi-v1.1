97f0c008d4838e8a69bd518fc5e9066959ffc93f
khj <kim37171@cuk.edu>
2023-01-27 01:37:54 +0900

    fix: jwt 사용 및 createV2()와 updateV2() 메서드명 수정
    @CookieValue 어노테이션 사용
    createV2 -> create
    update -> update

--- src/main/java/com/gophagi/nanugi/groupbuying/controller/GroupbuyingController.java
+++ src/main/java/com/gophagi/nanugi/groupbuying/controller/GroupbuyingController.java
@@ -1,17 +1,18 @@
 package com.gophagi.nanugi.groupbuying.controller;
 
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.gophagi.nanugi.common.jwt.JwtTokenProvider;
 import com.gophagi.nanugi.groupbuying.constant.Category;
 import com.gophagi.nanugi.groupbuying.dto.GroupbuyingBoardDTO;
 import com.gophagi.nanugi.groupbuying.dto.GroupbuyingThumbnailDTO;
@@ -19,8 +20,6 @@ import com.gophagi.nanugi.groupbuying.service.GroupbuyingBoardCommandService;
 import com.gophagi.nanugi.groupbuying.service.GroupbuyingBoardQueryService;
 
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -36,38 +35,35 @@ public class GroupbuyingController {
 
 	@PostMapping(value = "${groupbuying.create-url}",
 		consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-	public void createV2(@RequestPart GroupbuyingBoardDTO dto,
+	public void create(@RequestPart GroupbuyingBoardDTO dto,
 		@RequestPart List<MultipartFile> files,
-		HttpSession session) {
-		Long userId = (Long)session.getAttribute("userId");
+		@CookieValue String token) {
+
+		Long userId = Long.parseLong(JwtTokenProvider.getUserNameFromJwt(token));
 		commandService.create(dto, files, userId);
 	}
 
-	//@PostMapping("${groupbuying.update-url}")
-//	public void update(@RequestBody GroupbuyingBoardDTO dto) {
-//		commandService.update(dto);
-//	}
+	@PostMapping(value = "${groupbuying.update-url}",
+		consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+	public void update(@RequestPart GroupbuyingBoardDTO dto,
+		@RequestPart(required = false) List<MultipartFile> files,
+		@RequestPart(required = false) List<Long> deletePhotoIdList,
+		@CookieValue String token) {
 
-	@PostMapping(value ="${groupbuying.update-url}",
-				 consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-	public void updateV2(@RequestPart GroupbuyingBoardDTO dto,
-										@RequestPart(required = false) List<MultipartFile> files,
-										@RequestPart(required = false) List<Long> deletePhotoIdList,
-										HttpSession session) {
-		Long userId = (Long) session.getAttribute("id");
-		commandService.update(dto, files,deletePhotoIdList, userId);
+		Long userId = Long.parseLong(JwtTokenProvider.getUserNameFromJwt(token));
+		commandService.update(dto, files, deletePhotoIdList, userId);
 
 	}
 
 	@PostMapping("${groupbuying.order-url}/{id}")
-	public void order(@PathVariable("id") Long id, HttpSession session) {
-		Long userId = (Long)session.getAttribute("userId");
+	public void order(@PathVariable("id") Long id, @CookieValue String token) {
+		Long userId = Long.parseLong(JwtTokenProvider.getUserNameFromJwt(token));
 		commandService.order(userId, id);
 	}
 
 	@PostMapping("${groupbuying.cancel-url}/{id}")
-	public void cancel(@PathVariable("id") Long id, HttpSession session) {
-		Long userId = (Long)session.getAttribute("userId");
+	public void cancel(@PathVariable("id") Long id, @CookieValue String token) {
+		Long userId = Long.parseLong(JwtTokenProvider.getUserNameFromJwt(token));
 		commandService.cancel(userId, id);
 	}
 
