7cd1dc7596d1074b1396b489e9953b833fcb033c
imsori <dlathfl868@naver.com>
2023-01-27 03:09:34 +0900

    fix: jwt토큰 쿠키에 저장

--- src/main/java/com/gophagi/nanugi/member/dto/LoginInfoDTO.java
+++ src/main/java/com/gophagi/nanugi/member/dto/LoginInfoDTO.java
@@ -1,9 +1,14 @@
 package com.gophagi.nanugi.member.dto;
 
+import com.gophagi.nanugi.member.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class LoginInfoDTO {
     private Long userId;
     private String nickname;
@@ -16,4 +21,11 @@ public class LoginInfoDTO {
         this.token = token;
     }
 
+    public static LoginInfoDTO toLoginInfoDTO(MemberDTO member,String token){
+        return LoginInfoDTO.builder()
+                .userId(member.getId())
+                .nickname(member.getNickname())
+                .token(token)
+                .build();
+    }
 }
