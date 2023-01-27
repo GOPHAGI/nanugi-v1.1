ddac8cadd58768208da16df85d7a7b65748470a4
khj <kim37171@cuk.edu>
2023-01-27 18:34:38 +0900

    docs, fix: ErrorCode 및 예외 클래스 추가 및 수정

--- src/main/java/com/gophagi/nanugi/groupbuying/exception/PersonnelLimitExceededException.java
+++ src/main/java/com/gophagi/nanugi/groupbuying/exception/PersonnelLimitExceededException.java
@@ -1,4 +1,11 @@
 package com.gophagi.nanugi.groupbuying.exception;
 
+import com.gophagi.nanugi.common.excepion.ErrorCode;
+
 public class PersonnelLimitExceededException extends RuntimeException {
+	private ErrorCode errorCode;
+	public PersonnelLimitExceededException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
 }
